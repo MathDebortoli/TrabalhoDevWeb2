a78c7a242a9c77623d1594e37019f11baca5f5f8
LeandroAmorim <leandroamorim0727@gmail.com>
2024-09-27 09:54:32 -0300

    add comp iniciais com bugs no html

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { AtorComponent } from '../ator/ator.component';
+import { InicioComponent } from '../inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' }, // Redireciona para 'inicio' na rota vazia
+  { path: 'inicio', component: InicioComponent }, // Rota para o componente inicial
+  { path: 'ator', component: AtorComponent } // Rota para o componente Ator
+];
