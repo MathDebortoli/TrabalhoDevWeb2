65a60b9d2a5053f9644b00e78c7235c87b2ac882
LeandroAmorim <leandroamorim0727@gmail.com>
2024-09-29 15:41:53 -0300

    Refactor toolbar and fix routing in app component

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 
@@ -11,12 +11,35 @@ import {MatIconModule} from '@angular/material/icon';
     RouterOutlet,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    RouterLink,
+    RouterLinkActive,
 
   ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+
+  template:`
+  <mat-toolbar color="primary">
+  <button mat-button [routerLink]="['/']">
+    <mat-icon>home</mat-icon> Início
+  </button>
+  <button mat-button [routerLink]="['/ator']">
+    Ator
+  </button>
+  <button mat-button>
+    Classe
+  </button>
+  <button mat-button>
+    Diretor
+  </button>
+</mat-toolbar>
+    <router-outlet></router-outlet>
+    
+`,
+  styleUrls: ['./app.component.scss']
+
 }
+
+
 )
 export class AppComponent {
   title = 'trabalho-angular';
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+
 import { Routes } from '@angular/router';
+
+//Paginas
 import { AtorComponent } from '../ator/ator.component';
 import { InicioComponent } from '../inicio/inicio.component';
 
+
 export const routes: Routes = [
-  { path: '', redirectTo: 'inicio', pathMatch: 'full' }, // Redireciona para 'inicio' na rota vazia
-  { path: 'inicio', component: InicioComponent }, // Rota para o componente inicial
-  { path: 'ator', component: AtorComponent } // Rota para o componente Ator
+  { path: '',
+    component:InicioComponent
+   },
+
+  { path: 'ator',
+    component: AtorComponent
+  }
 ];
