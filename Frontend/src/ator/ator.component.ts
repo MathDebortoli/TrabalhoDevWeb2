c9bc298b8583f0c105f584cd9a315379ed42f977
leandroamorim07 <leandroamorim0727@gmail.com>
2024-09-30 13:39:16 -0300

    funcionando

--- Frontend/src/ator/ator.component.ts
+++ Frontend/src/ator/ator.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-ator',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './ator.component.html',
   styleUrl: './ator.component.scss'
 })
