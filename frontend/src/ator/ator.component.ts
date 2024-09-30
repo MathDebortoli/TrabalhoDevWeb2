ff394cb3d7090c86a21531603f8a2fe3b5a26393
leandroamorim07 <leandroamorim0727@gmail.com>
2024-09-30 14:31:18 -0300

    df

--- frontend/src/ator/ator.component.ts
+++ frontend/src/ator/ator.component.ts
@@ -3,13 +3,14 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-ator',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule,MatTableModule],
   templateUrl: './ator.component.html',
-  styleUrl: './ator.component.scss'//fgfgf
+  styleUrl: './ator.component.scss'
 })
 export class AtorComponent {
 
