66dd36c2000fadcd4e787b7c2584951d53bd364f
Matheus Debortoli <franciscodebortoli@hotmail.com.br>
2024-10-02 17:08:47 -0300

    Data

--- frontend/src/app/ator/ator.component.ts
+++ frontend/src/app/ator/ator.component.ts
@@ -4,15 +4,30 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079 },
+  { position: 2, name: 'Helium', weight: 4.0026 },
+  { position: 3, name: 'Lithium', weight: 6.941 },
+  { position: 4, name: 'Beryllium', weight: 9.0122 },
+];
 
 @Component({
   selector: 'app-ator',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule,MatTableModule],
+  imports: [FormsModule, MatIconModule, MatFormFieldModule, MatInputModule,MatButtonModule,MatTableModule],
   templateUrl: './ator.component.html',
   styleUrl: './ator.component.scss'
-  
+
 })
 export class AtorComponent {
-
+  displayedColumns: string[] = ['position', 'name', 'weight'];
+  dataSource = ELEMENT_DATA;
 }
--- frontend/src/app/classe/classe.component.ts
+++ frontend/src/app/classe/classe.component.ts
@@ -1,20 +1,45 @@
 import { Component } from '@angular/core';
 
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTableModule} from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079 },
+  { position: 2, name: 'Helium', weight: 4.0026 },
+  { position: 3, name: 'Lithium', weight: 6.941 },
+  { position: 4, name: 'Beryllium', weight: 9.0122 },
+];
 
 @Component({
   selector: 'app-classe',
   standalone: true,
-  imports: [FormsModule,MatNativeDateModule, MatDatepickerModule,MatFormFieldModule, MatInputModule,MatButtonModule,MatTableModule],
+  imports: [
+    FormsModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+  ],
   templateUrl: './classe.component.html',
   styleUrl: './classe.component.scss',
 })
-export class ClasseComponent {
 
+export class ClasseComponent {
+  displayedColumns: string[] = ['position', 'name', 'weight'];
+  dataSource = ELEMENT_DATA;
 }
