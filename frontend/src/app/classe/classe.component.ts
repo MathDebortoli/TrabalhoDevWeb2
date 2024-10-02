1bab70449f53b92e66d9d3849979db026e42d342
Matheus Debortoli <franciscodebortoli@hotmail.com.br>
2024-10-02 15:03:08 -0300

    aaaa

--- frontend/src/app/classe/classe.component.ts
+++ frontend/src/app/classe/classe.component.ts
@@ -5,15 +5,19 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
-import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
   selector: 'app-classe',
   standalone: true,
-  imports: [FormsModule,MatNativeDateModule,MatFormFieldModule, MatInputModule,MatButtonModule,MatTableModule,MatDatepickerModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [FormsModule,MatNativeDateModule, MatDatepickerModule,MatFormFieldModule, MatInputModule,MatButtonModule,MatTableModule],
   templateUrl: './classe.component.html',
-  styleUrl: './classe.component.scss'
+  styleUrl: './classe.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ClasseComponent {
 
