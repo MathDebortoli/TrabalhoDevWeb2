941afc839dcc968ccb54618fb1979177793c8171
Matheus Debortoli <franciscodebortoli@hotmail.com.br>
2024-10-02 14:53:46 -0300

    azul

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- frontend/src/app/classe/classe.component.ts
+++ frontend/src/app/classe/classe.component.ts
@@ -5,11 +5,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-classe',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule,MatTableModule],
+  imports: [FormsModule,MatNativeDateModule,MatFormFieldModule, MatInputModule,MatButtonModule,MatTableModule,MatDatepickerModule],
   templateUrl: './classe.component.html',
   styleUrl: './classe.component.scss'
 })
